93e09d79ce7ddb9a8445a21f29a2970fa8b09041
Pedro Henrique Lemes Da Silva <pedroca21265@gmail.com>
2021-04-19 15:48:49 +0000

    create GET and POST routes

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,30 @@ import express from 'express'
 
 const app = express()
 
+
+/**
+ * GET = Buscas
+ * POST = Criação
+ * PUT = Alteração
+ * DELETE = Excluir/Deletar
+ * PATCH = Alterar uma informação especifica
+ */
+
+
+// Rota GET para '/'
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'Olá NLW#05'
+  })
+})
+
+
+// Rota POST para '/users'
+app.post('/users', (request, response) => {
+  return response.json({
+    message: 'Usuário salvo com sucesso'
+  })
+})
+
+
 app.listen(3333, () => console.log('Server is running on port 3333'))
